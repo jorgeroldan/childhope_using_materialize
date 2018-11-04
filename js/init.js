b26efe81441f545d276f10a4a4d51efa699d0a83
Jorge Roldán <roldan21@gmail.com>
2018-11-04 15:02:25 -0300

    first version html

---
+++ js/init.js
@@ -0,0 +1,18 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+      
+
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
